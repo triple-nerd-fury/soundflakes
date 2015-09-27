f9d97ef117424225f00c9f1fa3e89d682eb0ed27
Robinson Lam <robinsonlam94@gmail.com>
2015-09-28 01:17:27 +1000

    Simple branch on click event

--- app/assets/javascripts/soundflakes-paper.js
+++ app/assets/javascripts/soundflakes-paper.js
@@ -1,44 +1,8 @@
 var drawLineTimers = []; // Array of timers.
 var branches = []; // Array of branches
 
-var newBranch = function( origin, direction ) { // Two objects that contain x and y coords
-  var path = new paper.Path();
-  path.strokeColor = 'black';
-  var start = new paper.Point( origin.x, origin.y );  
-  path.moveTo(start);
-
-  var branch = {
-    path: path,
-    start: start,
-    direction: direction
-  }
-
-  branches.push(branch);
-};
-
-var drawLine = function() {
-  var durationLine = {
-    x: 0,
-    y: 0,
-  };
-
-  newBranch( durationLine, { x: 1, y: 1 } );
-  newBranch( durationLine, { x: 3, y: 2 } );
-  newBranch( durationLine, { x: 3, y: 4 } );
-
-  var drawLineTimer = setInterval( function() {
-    for ( x = 0; x < branches.length; x++ ) {
-      branches[x].path.lineTo(branches[x].start.add(branches[x].direction));
-      branches[x].start = branches[x].start.add(branches[x].direction);
-    }
-    paper.view.draw();
-  }, 50);
-
-  drawLineTimers.push(drawLineTimer);
-};
-
+// CLICK EVENT
 var clickedFlag = false;
-
 var branchListener = function() {
   if (clickedFlag) {
     return true;
@@ -46,42 +10,71 @@ var branchListener = function() {
   return false;
 };
 
+var Branch = function( path, origin, direction ) {
+  this.path = path;
+  this.location = origin;
+  this.direction = direction;
+
+  this.listenForClick = function() {
+    console.log("ListenToClick");
+  };
+}
+
+var newBranch = function( origin, direction ) { // Two objects that contain x and y coords
+  var path = new paper.Path();
+  path.strokeColor = 'black';
+  var start = new paper.Point( origin.x, origin.y );  
+  path.moveTo(start);
+
+  var newBranch = new Branch( path, start, direction );
+  branches.push(newBranch);
+  console.log(branches);
+};
+
+var init = function() { // Initializes starting points/branches
+  var durationLine = { x: 0, y: 0 };
+  newBranch( durationLine, { x: 1, y: 1 } );
+
+  renderLines();
+};
+
+var renderLines = function() {
+
+  var drawLineTimer = setInterval( function() {
+    for ( x = 0; x < branches.length; x++ ) {
+      var newLocation = branches[x].location.add(branches[x].direction);
+      branches[x].path.lineTo(newLocation);
+      branches[x].location = newLocation;
+      branches[x].listenForClick();
+
+      if ( branchListener() ) {
+        var randomX = Math.floor(Math.random() * 4) - 2;
+        var randomY = Math.floor(Math.random() * 4) - 2;
+        newBranch( { x: branches[x].location.x, y: branches[x].location.y }, { x: randomX, y: randomY } );
+        clickedFlag = false;
+      }
+    }
+    paper.view.draw();
+  }, 50);
+
+  drawLineTimers.push(drawLineTimer);
+};
+
 $(document).ready(function() {
-  console.log("Document Ready!");
-  
-$("#myCanvas").on('click', function() {
-  console.log("clicked!");
-  if (clickedFlag) {
-    clickedFlag = false;
-  } else {
-    clickedFlag = true;
-  }
-});
-
-
+  $("#myCanvas").on('click', function() {
+    console.log("clicked!");
+    if (clickedFlag) {
+      clickedFlag = false;
+    } else {
+      clickedFlag = true;
+    }
+  });
   paper.install(window);
 
   window.onload = function () {
     var canvas = document.getElementById("myCanvas");
     paper.setup(canvas);  // Sets up a PaperScope, initializes an empty project and a view.
-
-    // var path = new paper.Path(); // Creates a Path to draw a line
-    // path.strokeColor = 'black'; // Gives the stroke of the line a black color
-
-    // var start = new paper.Point(100, 100); // Sets a point with coordinates 100,100 as a variable 'start'
-
-    // path.moveTo(start); // Move to the specified point and draw a line from here
-    // path.lineTo(start.add([ 200, -50 ]));  // Add 200 to x and -50 to Y
-    // start = start.add([200, -50]); // Set new point
-    // path.lineTo(start.add([ 150, -10 ]));  // Add 150 to x and -10 to Y
-    // start = start.add([150, -10]); // Set new point
-    // path.lineTo(start.add([300, 100]));
-    // start = start.add([300, 100]); // set new point
-
-    // paper.view.draw(); // Render what we've document
-
-    drawLine();
-
+    init();
   };
-
 });
+

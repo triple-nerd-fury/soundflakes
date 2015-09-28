079296b0fe8e7eb28b966665635a7a5b2244a9ca
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-09-28 23:32:04 +1000

    update visualiser

--- app/assets/javascripts/backbone/views/SnowFlakeView.js
+++ app/assets/javascripts/backbone/views/SnowFlakeView.js
@@ -34,6 +34,8 @@ app.SnowFlakeView = Backbone.View.extend({
 		  document.getElementById('audio_box').appendChild(audio);
 		  context = new AudioContext(); // AudioContext object instance
 		  analyser = context.createAnalyser(); // AnalyserNote method
+		  analyser.maxDecibels = -10;
+		  analyser.minDecibels = -100;
 		  canvas = document.getElementById('analyser_render');
 		  ctx = canvas.getContext('2d');
 		  // Re-route audio playback into the processing graph of the audio context
@@ -51,10 +53,10 @@ app.SnowFlakeView = Backbone.View.extend({
 		  analyser.getByteFrequencyData(fbc_array);
 		  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
 		  ctx.fillStyle = '#00CCFF'; // Color of the bars
-		  bars = 100;
+		  bars = 1024;
 		  for (var i = 0; i < bars; i++) {
-		    bar_x = i * 3;
-		    bar_width = 2;
+		    bar_x = i;
+		    bar_width = 1;
 		    bar_height = -(fbc_array[i] / 2);
 		    //  fillRect( x, y, width, height ) // Explanation of the parameters below
 		    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);

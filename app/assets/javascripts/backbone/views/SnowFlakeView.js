8510614e888fa7f011906e8d75f694d217484d1d
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-10-02 11:00:09 +1000

    add spin, mobile compatability

--- app/assets/javascripts/backbone/views/SnowFlakeView.js
+++ app/assets/javascripts/backbone/views/SnowFlakeView.js
@@ -1,139 +1,18 @@
 var app = app || {};
-var throttledGenerate;
-
-var branchAmount = 5;
-var branchMaxSpeed = 0.01;
-var lastShortAvg = 0;
-var lastLongAvg = 0;
-var avg = 0;
-var currentHighest = [];
-var longHistoryArray =[];
+window.requestAnimFrame = (function(){ 
+    return (
+        window.requestAnimationFrame       || 
+        window.webkitRequestAnimationFrame || 
+        window.mozRequestAnimationFrame    || 
+        window.oRequestAnimationFrame      || 
+        window.msRequestAnimationFrame     || 
+        function(/* function */ callback){
+            window.setTimeout(callback, 1000 / 60);
+        }
+    );
+})();
 var moveFlake = 0;
-var count = 0;
 
-// var Branch = function( path, origin, direction ) {
-//   this.path = path;
-//   this.location = origin;
-//   this.direction = direction;
-
-//   this.move = function(fbc_array) {
-//     var newLocation = this.location.add(this.direction);
-//     this.path.lineTo(newLocation);
-//     this.location = newLocation;
-    
-//     this.checkFbc(fbc_array);
-//     this.checkLimits();
-//   };
-
-//   this.generateNewBranches = function() {  
-//   	// console.log("Branch Generated");
-//   	// for ( x = 0; x < branchAmount; x++ ) {
-//    	//    var index = Math.floor(Math.random() * app.snowFlakeView.branches.length); // select random branch from available branches
-//    	//    if ( app.snowFlakeView.branches[index] ) { // if branch exists in random index
-//   	for( x = 0; x < 1; x++ ) {
-//     		var newOrigin = {
-// 	  			x: app.snowFlakeView.branches[0].location.x,
-// 	  			y: app.snowFlakeView.branches[0].location.y
-// 	  		};
-// 	  		var newDirection = {
-// 	  			x: (Math.random() * 0.6) - 0.3,
-// 	  			y: (Math.random() * 0.6) - 0.3
-// 	  		};
-// 	  		app.snowFlakeView.newBranch( newOrigin, newDirection );
-//       }
-//     // }
-//   };
-
-//   if ( !throttledGenerate ) { throttledGenerate = _.throttle( this.generateNewBranches, 1000 ); }
-  
-//   this.checkFbc = function(fbc_array) {
-//   	var total = 0;
-//   	for ( var x = 0; x < fbc_array.length; x++ ) {
-//   		total += fbc_array[x];
-//   	}
-//   	total = total / fbc_array.length;
-
-//     if (!currentHighest.length){
-//       currentHighest.push(total)
-//     }
-
-//     if ((currentHighest[0] - 5) < total) {
-//       // app.myCircle.scale( 5 );
-//       currentHighest[0] = total
-//       // throttledGenerate();
-//     }
-//     // if ( Math.random() <= 0.5 ) {
-//     //   app.myCircle.scale( 2 );
-//     // } else {
-//     //   // debugger;
-//     //   app.myCircle.scale( 0.5 );
-//     // }
-
-//     // console.log(currentHighest)
-
-//  //    if (total > 70){
-//  //   console.log(total)
-//  // }
-//   //   lastShortAvg = (5 * lastShortAvg + total)/6
-   
-//   //   // if (count > 100) {
-//   //   //   var compare = lastShortAvg/lastLongAvg
-//   //   //   if (compare > 1.3) {   
-//   //   //     console.log(compare)
-//   //   //   }
-//   //   // } 
-
-//   //   lastLongAvg = (29 * lastLongAvg + total)/30
-
-
-//      if ( total/avg > 1.3 ) { 
-//      throttledGenerate();
-//    // console.log(total/avg)
-//     }
-
-//      // historyArray.push(total)
-
-//   //   if (count % 100 == 0) {
-//   //     longHistoryArray.push(historyArray)
-//   //     }
-//   //     if (historyArray.length > 100) {
-//   //     historyArray.shift();
-//   //   }
-   
-
-//   // if (longHistoryArray.length > 1000) {
-//   //   longHistoryArray.shift();
-//   // } 
-//   // //  if (count % 1000 == 0) {
-//   // //   superLongHistoryArray.push(longHistoryArray);
-//   // // } 
-
-//     avg = total
-//   //   count += 1
-//   //   var shortHistory = 0;
-//   //   for (i = 0; i < historyArray.length; i++) {
-//   //     shortHistory += historyArray[i];
-//   //   }
-//   //    console.log('short' + shortHistory/historyArray.length )
-
-//   //    var longHistory = 0;
-//   //   for (i = 0; i < longHistoryArray.length; i++) {
-//   //     for (j = 0; j < longHistoryArray[i].length; j++){
-//   //     longHistory += longHistoryArray[i][j];
-//   //     }
-//   //   }
-//   //    console.log('long' + longHistory)
-//   };
-
-//   this.checkLimits = function() {
-//   	var xLimit = $('#analyser_render').width();
-// 	  var yLimit = $('#analyser_render').height();
-//   	if ( this.location.x <= 0 || this.location.y <= 0 || this.location.x >= xLimit || this.location.y >= yLimit) {
-//   		this.direction.x = 0;
-//   		this.direction.y = 0;
-//   	}
-//   };
-// }
 
 app.SnowFlakeView = Backbone.View.extend({
 	el: '#main',
@@ -158,91 +37,8 @@ app.SnowFlakeView = Backbone.View.extend({
 	},
 
 
-	// newBranch: function( origin, direction ) { // Two objects that contain x and y coords
-	// 	  var path = new paper.Path();
-	// 	  var color = '#' + ( function co(lor) { return (lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && (lor.length == 6) ?  lor : co(lor); })('');
-	// 	  path.strokeColor = color;
-	// 	  var start = new paper.Point( origin.x, origin.y );  
-	// 	  path.moveTo(start);
-
-	//   var newBranch = new Branch( path, start, direction );
-	//   this.branches.push(newBranch);
-	// },
-
-  // drawInitialBeatCircle: function () {
-  //   var centerHorizontal = parseInt( $("canvas").width() ) / 2;
-  //   var centerVertical = parseInt( $("canvas").height() ) / 2;
-
-  //   app.beatCircle = new Path.Circle({
-  //       center: new Point( centerHorizontal, centerVertical ),
-  //       radius: 35,
-  //       fillColor: 'green',
-  //       strokeColor: 'green'
-  //   });
-
-  //   paper.view.draw();
-  // },
-
-//   scaleBeatCircle: function ( number ) {
-//     if ( app.beatCircle ) {
-//       var desired  = number;
-//       var original = 1 / desired;
-//       var originalRadius = app.beatCircle.bounds.width / 2;
-//       var segmentsOfAnimation = ( desired - original ) / 20;
-
-//       console.log( "Desired Scale: ", desired );
-//       console.log( "Original Scale: ", original );
-//       console.log( "Segments of Animation: ", segmentsOfAnimation );
-//       var centerHorizontal = parseInt( $("canvas").width() ) / 2;
-//       var centerVertical = parseInt( $("canvas").height() ) / 2;
-
-//       var animationInterval = window.setInterval(function () {
-//         app.beatCircle.bounds.width = app.beatCircle.bounds.width * ( segmentsOfAnimation + 1 );
-//         app.beatCircle.bounds.height = app.beatCircle.bounds.height * ( segmentsOfAnimation + 1 ); 
-//         app.beatCircle.position = new Point( centerHorizontal, centerVertical );
-//         paper.view.draw();
-//       }, 100);
-
-//       window.setTimeout(function () {
-//         window.clearInterval( animationInterval );
-//         window.setTimeout( function () {
-//           app.beatCircle.bounds.width = originalRadius * 2;
-//           app.beatCircle.bounds.height = originalRadius * 2; 
-//           app.beatCircle.position = new Point( centerHorizontal, centerVertical );  
-
-//           paper.view.draw();
-//         }, 250);
-//       }, 2000);
-
-//       // for ( var i = 1; i < 20; i++ ) {
-//         // Immediately Invoked Function Expression (IIFE) - Douglas Crockford is the man
-//         // (function (i) {
-//         //   window.setTimeout(function () {
-//         //     app.beatCircle.bounds.width = app.beatCircle.bounds.width * ( segmentsOfAnimation + 1 );
-//         //     app.beatCircle.bounds.height = app.beatCircle.bounds.height * ( segmentsOfAnimation + 1 ); 
-//         //     app.beatCircle.position = new Point( centerHorizontal, centerVertical );
-//         //     paper.view.draw();
-//         //   }, 100);
-//         // })(i);
-//       // }
-// // 
-//       // app.beatCircle.scale( desired );
-//       // paper.view.draw();
-
-
-//       // Sort out the initial ratio
-//         // With number as the desired size increase
-//     }
-//   },
-
   visualiser: function() {
     playing = true;
-    // app.newPath = new Path();
-    // app.start = new Point(50,100);
-    // app.end = new Point(50, 500);
-    // app.newPath.add(app.start);
-    // app.newPath.add(app.end);
-    // app.newPath.strokeColor = 'pink';
 
     if ( app.newPath_one ) {
       app.newPath_one.remove();
@@ -411,19 +207,13 @@ app.SnowFlakeView = Backbone.View.extend({
 
     app.snowflake = new app.paper.Group([app.groupVertical, app.groupSixty, app.groupOneTwenty])
   
-    app.snowflake.onMouseDown = function(event) {
+    app.snowflake.onMouseDown = function() {
       app.copy = this.clone();
-      app.copy.scale(0.2,0.2);
+            app.copy.scale(0.2,0.2);
       var x = Math.floor(Math.random() * $("canvas").width())
       var y = Math.floor(Math.random() * $("canvas").height())
-      app.copy.position = new app.paper.Point( 100, 100 );
-
-      app.myCircle = new app.paper.Path.Circle(new app.paper.Point(100, 70), 5);
-      app.myCircle.fillColor = 'red';
-
-      // console.log( app.copy, app.copy.position );
-      // console.log('click')
-      app.paper.view.draw();
+      app.copy.position = new app.paper.Point(x,y);
+      console.log(app.copy);
     } 
    app.paper.view.draw();
   },
@@ -468,9 +258,8 @@ app.SnowFlakeView = Backbone.View.extend({
 	},
 
   paperFrame: function() {
-    // On each frame, rotate the path by 3 degrees:
-    // path.rotate(3);
-    if ( $('#snowflake-visual').is(':checked') ){
+   
+      if ( $('#snowflake-visual').is(':checked') ){
       app.paper.project.clear()
       app.snowFlakeView.drawSnowflake();
     }
@@ -478,15 +267,14 @@ app.SnowFlakeView = Backbone.View.extend({
       app.paper.project.clear()
       app.snowFlakeView.visualiser();
     }
-
-   //  if (app.snowflake) {
-   //  app.snowflake.rotate(moveFlake);
-   //   moveFlake += 1;
-   // }
-  },
+    if (app.snowflake) {
+    app.snowflake.rotate(moveFlake);
+     moveFlake += 0.5;
+   }
+    },
 
 	frameLooper: function() {
-		window.requestAnimationFrame( this.frameLooper.bind(this) );
+		window.requestAnimFrame( this.frameLooper.bind(this) );
 
     app.paper.view.on({
       frame: this.paperFrame
@@ -495,25 +283,6 @@ app.SnowFlakeView = Backbone.View.extend({
 	  this.fbc_array = new Uint8Array(this.analyser.frequencyBinCount);
 
 	  this.analyser.getByteFrequencyData(this.fbc_array);
-    // debugger;
-	  // console.log(this.context.currentTime)
-	  // ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-	  // ctx.fillStyle = '#00CCFF'; // Color of the bars
-	  // bars = 1024;
-	  // for (var i = 0; i < bars; i++) {
-	  //   bar_x = i;
-	  //   bar_width = 1;
-	  //   bar_height = -(fbc_array[i]);
-	  //   //  fillRect( x, y, width, height ) // Explanation of the parameters below
-	  //   ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-	  // }
-  	// if (!app.snowFlakeView.audio.paused) {
-   //    for ( x = 0; x < this.branches.length; x++ ) {
-   //    	var newLocation = this.branches[x].location.add(this.branches[x].direction);
-   //    	this.branches[x].move(this.fbc_array);
-   //    }
-   //   	paper.view.draw();
-   //  }
 	},
 
 	startMusic: function() {
